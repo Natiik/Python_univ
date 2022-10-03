794b917d5a86dc899d0e0ec46410343fbda46201
AnastasiiaKomar <komaranastasiaa@gmail.com>
2022-10-04 00:33:34 +0300

    error handling improved

--- Calculate 3/task_2.py
+++ Calculate 3/task_2.py
@@ -1,17 +1,19 @@
 import sys
 
 maximum = - sys.maxsize
+is_error = False
 
 for i in range(3):
     try:
         input_value = input('Write value ->')
         maximum = max(int(input_value), maximum)
     except ValueError:
+        is_error = True
         print('Not a number, can\'t calculate max from 3 numbers')
         break
 
-print('Maximum input value is', maximum)
-
+if not is_error:
+    print('Maximum input value is', maximum)
 
 # in case you want to make the program endless here is the cose sample that will work until input is not numeric value:
 # import sys
